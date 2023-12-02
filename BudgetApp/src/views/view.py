22fb16f0ef9ac305d76451bc1f0e8096ed7ede51
keranenkirill <keranen.kirill@gmail.com>
2023-12-02 16:36:56 +0200

    view

---
+++ BudgetApp/src/views/view.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+from tkinter import ttk
+
+
+class View(tk.Frame):
+
+    def __init__(self, parent, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+        self.style = ttk.Style(self)
+
+    def remove(self):
+        self.pack_forget()
