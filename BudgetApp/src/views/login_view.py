f662475a51a0ccf7e6acabc4cdd6f1137b37578c
keranenkirill <keranen.kirill@gmail.com>
2023-12-02 16:35:25 +0200

    kirjautumisnäkymä

---
+++ BudgetApp/src/views/login_view.py
@@ -0,0 +1,65 @@
+import tkinter as tk
+from views.view import View  # pylint: disable=import-error
+
+
+class LoginView(View):
+
+    def __init__(self, parent, *args, account="", **kwargs):
+        super().__init__(parent, *args, **kwargs,  bg="#49183A")
+        self.account = account
+        self.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+        self.widgets()
+
+    def widgets(self):  # pylint: disable=too-many-statements
+        loginform = View(self, bg="#49183A")
+        loginform.pack(side=tk.TOP, anchor=tk.CENTER, padx=10, pady=50)
+
+        # user
+        usrlabel = tk.Label(loginform, text="Username:")
+        usrentry = tk.Entry(loginform, width=50)
+
+        usrlabel.grid(row=0, column=0, padx=10, pady=10)
+        usrentry.grid(row=0, column=1, padx=10, pady=10)
+
+        # password
+        pwdlabel = tk.Label(loginform, text="Password:")
+        pwdentry = tk.Entry(loginform, width=50, show="*")
+
+        pwdlabel.grid(row=1, column=0, padx=10, pady=10)
+        pwdentry.grid(row=1, column=1, padx=10, pady=10)
+
+        self.revealvar = tk.BooleanVar()
+        reveal = tk.Checkbutton(loginform, var=self.revealvar,
+                                text="reveal password", command=self.reveal_actions)
+        reveal.grid(row=2, column=0, padx=10, pady=10)
+
+        btn_register_user = tk.Button(
+            master=loginform, text="No Account? Register here.", command=self.register_action)
+        btn_register_user.grid(row=3, column=1, padx=10, pady=10)
+
+        btn_login_user = tk.Button(
+            master=loginform, text="Login", command=self.login_action)
+        btn_login_user.grid(row=3, column=2, columnspan=1, padx=10, pady=10)
+
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
+
+        self.username = usrentry
+        self.password = pwdentry
+
+        self.username.insert(0, self.account)
+        self.pack(side=tk.LEFT, fill=tk.BOTH, pady=2, expand=True)
+
+    def reveal_actions(self):
+        if self.revealvar.get():
+            self.password.config(show="")
+        else:
+            self.password.config(show="*")
+
+    def login_action(self):
+        if self.master.controller.login(self.username.get(), self.password.get()):
+            pass
+            # lets goo next logged view
+
+    def register_action(self):
+        self.master.controller.load_register_view()
