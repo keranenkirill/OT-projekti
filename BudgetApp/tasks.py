92fd826c644459e6a5476e0abb6915aa0ade8d53
keranenkirill <keranen.kirill@gmail.com>
2023-12-02 17:24:32 +0200

    tehtävien määritteleminen

--- BudgetApp/tasks.py
+++ BudgetApp/tasks.py
@@ -1,5 +1,32 @@
 from invoke import task
 
+
 @task
 def start(ctx):
     ctx.run("python3 src/app.py", pty=True)
+
+
+@task
+def build(ctx):
+    ctx.run("python3 src/build.py", pty=True)
+
+
+@task
+def test(ctx):
+    ctx.run("pytest src", pty=True)
+
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest src", pty=True)
+
+
+@task
+def coveragehtml(ctx):
+    ctx.run("coverage run --branch -m pytest src", pty=True)
+    ctx.run("coverage html", pty=True)
+
+
+@task
+def lint(ctx):
+    ctx.run("pylint src", pty=True)
