d845da5b95895a593aee0878f625ac65b66e0e59
keranenkirill <keranen.kirill@gmail.com>
2023-12-12 21:39:10 +0200

    upd

--- BudgetApp/src/views/view.py
+++ BudgetApp/src/views/view.py
@@ -7,6 +7,6 @@ class View(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         super().__init__(parent, *args, **kwargs)
         self.style = ttk.Style(self)
-
+        
     def remove(self):
         self.pack_forget()
